d21f92f82e318c16171d02e69c15975387edbe9b
asiri <asiribr@gmail.com>
2023-05-07 01:40:02 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="bg-[#f1f5f9] min-h-[100vh] ">
       <TodoList/>
-      <h1 className="text-center">Made by Asiri with his big brain</h1>
+      <h1 className="text-center">Made by Asiri with his big</h1>
     </div>
   )
 }
